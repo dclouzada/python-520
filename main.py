e6f91fe50102d059ab434cf2b5a48ef3bcc732fb
leonardo.mendes@4linux.com.br <leonardo.mendes@4linux.com.br>
2019-04-06 16:25:39 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,56 @@
+usuario = {
+	'nome': input('Digite seu nome'),
+	'idade': input('Digite sua idade'),
+	'email': input('Digite seu email'),
+}
+nome = usuario['nome']
+print('Usuario {} cadastrado com sucesso!'.format(usuario))
+
+
+
+
+
+
+
+
+
+
+
+
+exit()
+
+GRAVIDADE = 9.8
+
+primeiro_nome = 'David'
+segundo_nome = 'Castro'
+ultimo_nome = 'Louzada'
+
+idade = '25'
+idade = 25
+
+professor = True
+
+variavel = 'string'
+print(type(variavel))
+
+variavel = 121
+print(type(variavel))
+
+variavel = True
+print(type(variavel))
+
+
+exit() 
+
+print('hello, word')
+condicao = 'David Castro Louzada'
+print(type(condicao))
+condicao = 25
+print(type(condicao))
+condicao = True
+print(type(condicao))
+
+if condicao:
+	print('verdade')
+else:
+	erro
